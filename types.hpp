c390068d3a270e4bca26ef573d853777aed4c790
tom <tom@ce2316a1-0b13-4e9d-b0f6-5cad04d5ee9d>
2009-10-28 20:19:08 +0000

    set propset for dates etc

--- types.hpp
+++ types.hpp
@@ -30,7 +30,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #ifndef TYPES_INCL
 #define TYPES_INCL
 
-#define VERSION " 1.0 $Rev:     $ "
+#define VERSION " 1.0 $Rev$ "
 
 #define WRITE_TO_SCREEN true
 #define WRITE_TO_FILE true
