914b512fd65f3bee7d8cac6745bb5aedd56a8d0c
Tom McCombes <tom.mccombes@strath.ac.uk>
2013-05-27 14:10:10 +0100

    Setting up body panel interpolation rather than direct velocity calc

--- include/utils.hpp
+++ include/utils.hpp
@@ -54,6 +54,7 @@ public:
 
     static unsigned long int cpu_t;
     static int NumCellGaussPts;
+
     static inline Array < Array <double> > zeros(int a, int b) {
         return Array < Array <double> > (a, Array <double> (b, 0.0));
     }
@@ -66,13 +67,12 @@ public:
         return Array < Array <Vect3> > (a, Array <Vect3 > (b, 0.0));
     }
 
-
     template <typename T> static ARRAY3(T) zeros(int a, int b, int c) {
-        return ARRAY3(T) (a, ARRAY2(T) (b, Array <T> (c, 0.0)));
+        return ARRAY3(T) (a, ARRAY2(T) (b, Array <T > (c, 0.0)));
     }
 
     static inline Array < Array < Array <Vect3> > > zerosv(int a, int b, int c) {
-        return Array < Array <Array <Vect3> > > (a, Array <Array <Vect3> > (b, Array <Vect3> (c, Vect3(0.0))));
+        return Array < Array <Array <Vect3> > > (a, Array <Array <Vect3> > (b, Array <Vect3 > (c, Vect3(0.0))));
     }
 
     template<class T>
@@ -130,7 +130,7 @@ public:
 
     static void write1D(string varname, string fname, string &input, int m);
     static void WriteMATLABString(string vname, string fname, string data);
-    
+
     static void ReadBinaryVect3(Array <Vect3> &, string);
 
     inline static double rad2deg(double theta) {
@@ -145,9 +145,9 @@ public:
     static double interp2(Array<Array<double> > &X, Array<Array<double> > &Y, Array<Array<
             double> > &Z, double Xi, double Yi);
 
-    static double interp3(Array < Array<Array<double> > > &X,  Array < Array<Array<double> > > &Y, Array <Array<Array<
+    static double interp3(Array < Array<Array<double> > > &X, Array < Array<Array<double> > > &Y, Array <Array<Array<
             double> > > &Z, double Xi, double Yi, double Zi);
-    
+
     static Array <REAL> globalLinspace(REAL start, REAL end, int n);
     static Array <PANEL> Pans;
     static Array <Vect3> globalLinspace(Vect3 start, Vect3 end, int n);
@@ -160,11 +160,62 @@ public:
     static void GetCellPans();
     static Array <REAL> QuadPts, QuadWts;
     static void lgwt(int N, Array <REAL> &x, Array <REAL> &w);
-    template <typename T> static T interp3(ARRAY3(Vect3) &X, ARRAY3(T) &U, Vect3 Xi);
-    
-    
-    
+
+    template <typename T> static T interp3(ARRAY3(Vect3) & X, ARRAY3(T) & U, Vect3 Xi) {
+
+        //	Find dx, dy, dz
+        Vect3 DX(X[1][0][0].x - X[0][0][0].x, X[0][1][0].y - X[0][0][0].y, X[0][0][1].z - X[0][0][0].z);
+
+
+        //	Find nx, ny, nz
+        Vect3 Nx = (Xi - X[0][0][0]) / DX;
+
+        //	Lower index
+        int x0 = floor(Nx.x);
+        int y0 = floor(Nx.y);
+        int z0 = floor(Nx.z);
+
+        //	Upper index
+        int x1 = x0 + 1;
+        int y1 = y0 + 1;
+        int z1 = z0 + 1;
+
+        //	Position between indices
+        Vect3 DeltaX = Nx - floor(Nx);
+
+        //	Corner values
+        T u000 = U[x0][y0][z0];
+        T u010 = U[x0][y1][z0];
+        T u100 = U[x1][y0][z0];
+        T u110 = U[x1][y1][z0];
+
+        T u001 = U[x0][y0][z1];
+        T u011 = U[x0][y1][z1];
+        T u101 = U[x1][y0][z1];
+        T u111 = U[x1][y1][z1];
+
+
+
+        //   Get 4 corner z gradients and interpolate for corner values
+        T u00 = u000 + DeltaX.z * (u001 - u000) / (z1 - z0);
+        T u01 = u010 + DeltaX.z * (u011 - u010) / (z1 - z0);
+        T u10 = u100 + DeltaX.z * (u101 - u100) / (z1 - z0);
+        T u11 = u110 + DeltaX.z * (u111 - u110) / (z1 - z0);
+
+
+
+        //	Get 2 edge y gradients and interpolate for corner values
+        T u0 = u00 + DeltaX.y * (u01 - u00) / (y1 - y0);
+        T u1 = u10 + DeltaX.y * (u11 - u10) / (y1 - y0);
+
+
+        //	Get 1 x gradient and interpolate for p values
+        return u0 + DeltaX.x * (u1 - u0) / (x1 - x0);
+
+
+    };
+
 };
 /**************************************************************/
- 
+
 #endif	/* if !defined(UTILS_INCL) */
