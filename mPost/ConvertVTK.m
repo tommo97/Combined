48ccb58eed845e81e19baf5566b470fdedbe89fa
Tom McCombes <tom.mccombes@strath.ac.uk>
2013-06-11 21:40:29 +0100

    Added new .mat file post-processor. Idea is to get files down to a sensible size for ftping off HPC

--- mPost/ConvertVTK.m
+++ mPost/ConvertVTK.m
@@ -1,8 +1,8 @@
-clear all
-close all
-clc
-fname = 'RunData_000001.mat';
-load(fname,'CellPos','CellOms','GambitScale');
+function ConvertVTK(infname)
+fname = infname;
+vtkname = [infname(1:(length(infname) - 3)) 'vtk'];
+dirname = [infname(1:(length(infname) - 4)) '_VTKdata'];
+load(fname,'CellPos','CellOms','GambitScale','Time');
 Origin = min(CellPos)/GambitScale;
 
 minx = min(CellPos(:,1));
@@ -21,7 +21,11 @@ inds = sub2ind(size(Domain),subs(:,1),subs(:,2),subs(:,3));
 
 Domain(inds) = sqrt(CellOms(:,1).^2 + CellOms(:,2).^2 + CellOms(:,3).^2);
 
-writeVTK(Domain,'Wake100_1sec.NegStretch.full.vtk',1/GambitScale,Origin);
+wakeVTKname = [dirname '/Wake_Scale=' num2str(GambitScale) '_Time=' num2str(Time) '_' vtkname];
+
+mkdir(dirname)
+
+writeDomainVTK(Domain,wakeVTKname,1/GambitScale,Origin);
 load(fname,'Cp','NParts','AllBodyPoints_x','AllBodyPoints_y','AllBodyPoints_z');
  i = 1;
 eval(['load(fname,''TRANS1_' num2str(i-1) '_x'');']);
@@ -63,8 +67,8 @@ for i = 1:NParts
     Body{i}.Y = eval(['AllBodyPoints_y(BodyMainPointIDS' num2str(i-1) ')/GambitScale' ]);
     Body{i}.Z = eval(['AllBodyPoints_z(BodyMainPointIDS' num2str(i-1) ')/GambitScale' ]);
     Body{i}.Cp = eval(['Cp(BodySurface' num2str(i-1) ')']);
-    
-    eval(['vtkname = ''BladeData' num2str(i-1) '.vtk''']);
+    str = ['vtkname = [''' dirname '/BodyData_Scale=' num2str(GambitScale) '_Time=' num2str(Time) '_Body=' num2str(i-1) '.vtk'']'];
+    eval(str);
     
    
     
@@ -144,4 +148,65 @@ for i = 1:NParts
         toc
         
         
-    end
+end
+    
+
+function [] = writeDomainVTK(vol,vtkfile,spacing,origin)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Usage: writeVTK(vol,vtkfile)
+%
+%   vol:     The 3D matrix to be saved to file
+%   vtkfile: The output filename (string)
+%   notes:   Only writes binary STRUCTURED_POINTS
+%  
+% Erik Vidholm 2006
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% dimensions
+volinfo = whos('vol');
+
+sz = volinfo.size;
+X = sz(1); Y = sz(2); Z = 1;
+if( length(sz) == 3 )
+  Z = sz(3);
+end
+
+% open file (OBS! big endian format)
+fid = fopen(vtkfile,'w','b');
+
+% write header
+fprintf(fid, '%s\n', '# vtk DataFile Version 3.0');
+fprintf(fid, '%s\n', 'created by writeVTK (Matlab implementation by Erik Vidholm)');
+fprintf(fid, '%s\n', 'BINARY');  
+fprintf(fid, '%s\n', 'DATASET STRUCTURED_POINTS');  
+fprintf(fid, '%s%d%c%d%c%d\n', 'DIMENSIONS ', X, ' ', Y, ' ', Z);
+fprintf(fid, '%s%f%c%f%c%f\n', 'ORIGIN ', origin(1), ' ', origin(2), ' ', origin(3)); 
+fprintf(fid, '%s%f%c%f%c%f\n', 'SPACING ', spacing, ' ', spacing, ' ', spacing); 
+fprintf(fid, '%s%d\n', 'POINT_DATA ', X*Y*Z);
+
+tp = volinfo.class;
+if( strcmp(tp, 'uint8') > 0 )
+  fprintf(fid, '%s\n', 'SCALARS image_data unsigned_char');
+elseif( strcmp(tp, 'int8') > 0 )
+  fprintf(fid, '%s\n', 'SCALARS image_data char');
+elseif( strcmp(tp, 'uint16') > 0 )
+  fprintf(fid, '%s\n', 'SCALARS image_data unsigned_short');
+elseif( strcmp(tp, 'int16') > 0 )
+  fprintf(fid, '%s\n', 'SCALARS image_data short');
+elseif( strcmp(tp, 'uint32') > 0 )
+  fprintf(fid, '%s\n', 'SCALARS image_data unsigned_int');
+elseif( strcmp(tp, 'int32') > 0 )
+  fprintf(fid, '%s\n', 'SCALARS image_data int');
+elseif( strcmp(tp, 'single') > 0 )
+  fprintf(fid, '%s\n', 'SCALARS image_data float');
+elseif( strcmp(tp, 'double') > 0 )
+  fprintf(fid, '%s\n', 'SCALARS image_data double');
+end
+
+fprintf(fid, '%s\n', 'LOOKUP_TABLE default');
+
+% write data as binary
+fwrite(fid,vol,tp);
+
+% close file
+fclose(fid);
